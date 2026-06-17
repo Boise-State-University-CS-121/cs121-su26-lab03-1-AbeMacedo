9dd726ad6b23f341232a050589d24b3f1de89868
Abe Macedo <abemacedo@u.boisestate.edu>
2026-06-16 20:20:19 -0600

    SphereCalculator.java v1.3

--- SphereCalculator/SphereCalculator.java
+++ SphereCalculator/SphereCalculator.java
@@ -6,15 +6,22 @@ public class SphereCalculator
 public static void main(String[] Args) 
 
     {
-
+      /* Scanner for Radius */
       Scanner keyBrd = new Scanner(System.in);
 
-      System.out.println("Enter the sphere's radius: ");
-      double sphereRadius = keyBrd.nextDouble(); 
-      System.out.printf("Volume: " + "\n");
-      double sphereVolume = keyBrd.nextDouble();
-      System.out.printf("Surface Area: " + "\n");
-      double sphereSArea = keyBrd.nextDouble();
+      /* Initialized Variables */
+      int sphereRadius;
+      int sphereVolume;
+      int sphereSArea;
+
+      /* User input for Radius */
+      System.out.print("Enter the sphere's radius: ");
+      sphereRadius = keyBrd.nextInt(); 
+
+      sphereVolume = (4 / 3) * Math.PI * ()
+
+      //System.out.printf("Volume: " + "\n");
+      //System.out.printf("Surface Area: " + "\n");
 
     }
     
