f4050f7a4e61df0fcfc543d324faf6fa340eced0
Abe Macedo <abemacedo@u.boisestate.edu>
2026-06-16 18:47:11 -0600

    Account Generator.java v1.1

--- AccountGenerator/AccountGenerator.java
+++ AccountGenerator/AccountGenerator.java
@@ -1,10 +1,18 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class AccountGenerator 
 {
     public static void main(String[] Args) 
     
     {
 
-        
+        Scanner kyBrd = new Scanner(System.in);
+        Random randGen = new Random();
+
+       System.out.println("Enter your first name: "); 
+       System.out.println("Enter your last name: "); 
+       System.out.println("Username: "); 
 
     }
     
