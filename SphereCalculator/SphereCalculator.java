650154b816cf17ff06cdac1c49638f0fb588e5d0
Abe Macedo <abemacedo@u.boisestate.edu>
2026-06-16 19:21:02 -0600

    SphereCalculator.java v1.1

--- SphereCalculator/SphereCalculator.java
+++ SphereCalculator/SphereCalculator.java
@@ -3,10 +3,12 @@ public class SphereCalculator
 
 public static void main(String[] Args) 
 
-{
+    {
 
+      System.out.printf("Enter the sphere's radius: ");
+      System.out.printf("Volume: ");
+      System.out.printf("Surface Area: ");
 
-    
-}
+    }
     
 }
