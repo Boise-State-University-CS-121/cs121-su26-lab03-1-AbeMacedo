e209a5090f0117b75a1235202ba6d768affca63e
Abe Macedo <abemacedo@u.boisestate.edu>
2026-06-16 19:01:17 -0600

    AccountGenerator.java v1.4

--- AccountGenerator/AccountGenerator.java
+++ AccountGenerator/AccountGenerator.java
@@ -1,28 +1,33 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
+
 public class AccountGenerator 
 {
     public static void main(String[] Args) 
     
     {
 
-        Scanner kyBrd = new Scanner(System.in);
-        Random randGen = new Random();
+      Scanner kyBrd = new Scanner(System.in);
+      Random randGen = new Random();
 
-       System.out.print("Enter your first name: ");
-       String firstName = kyBrd.nextLine();
-       firstName = firstName.toLowerCase();
+      /* Input and Conversion */
+      System.out.print("Enter your first name: ");
+      String firstName = kyBrd.nextLine();
+      firstName = firstName.toLowerCase();
        
        System.out.print("Enter your last name: ");
        String lastName = kyBrd.nextLine();
        lastName = lastName.toLowerCase();
 
+       /* Characters to Use */
        char first1CharFirstName = firstName.charAt(0);
-       String first5CharLastName = lastName.subString(0,5);
+       String first5CharLastName = lastName.substring(0,5);
 
 
-       System.out.println("Username: "); 
+       System.out.println("Username: ");
+
 
 
 
